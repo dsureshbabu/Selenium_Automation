cb18ed930599ed3424645813137cc00f4a3b4050
Sureshbabu <dsureshbabu@BANL15b18a2a7.local>
2017-05-18 12:47:48 +0530

    Initial Commit.

---
+++ src/test/java/com/selenium/tests/OpenGooglePage.java
@@ -0,0 +1,13 @@
+package com.selenium.tests;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class OpenGooglePage {
+
+	public static void main(String[] args) {
+		WebDriver driver = new FirefoxDriver();
+
+	}
+
+}
